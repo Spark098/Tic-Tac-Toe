4b65086488a113675e765a6cd9cf6049f80f3a8b
Subhrajit Mutsuddi <mutsuddisubhrajit01@gmail.com>
2022-05-29 19:31:35 +0530

    Add create-room and join-room functionality

--- public/script.js
+++ public/script.js
@@ -1,14 +1,88 @@
 var socket = io();
 
-socket.on('reflect', attr=> {
-  console.log(attr);
-  var sym='X';
-  document.querySelector(`[data-cell-index="${attr}"]`).innerHTML=`<p>${sym}</p>`;
-})
+const startScreen = document.getElementById('start-screen');
+const newGame = document.getElementById('new-game');
+const inputRoomId = document.getElementById('room-id');
+const createRoomBtn = document.getElementById('create-room-btn');
+const joinGame = document.getElementById('join-game');
+const inputJoinId = document.getElementById('join-room-id');
+const joinRoomBtn = document.getElementById('join-room-btn');
+const displayError = document.getElementById('display-error');
+const gameScreen = document.getElementById('game-screen');
+const nextPlayer = document.getElementById('next-player');
+const gameNtf = document.getElementById('game-notification');
+
+let canChoose = false;
+let playerOneConnected = false;
+let playerTwoConnected = false;
+let playerId = 0;
+let roomId = "";
+let myChoice = "";
+let enemyChoice = "";
+
 
 document.querySelectorAll(".cell").forEach((cell) => {
+
   cell.addEventListener("click", handleAnyClick);
 });
+
+createRoomBtn.addEventListener('click', () => {
+  let id = inputRoomId.value;
+
+  console.log(id);
+  if (id == "") {
+    displayError.style.display = 'block';
+    displayError.innerHTML = "<h2>Please enter a valid room ID</h2>";
+  } else {
+    displayError.style.display = 'none';
+    socket.emit('create-room', id);
+  }
+})
+
+joinRoomBtn.addEventListener("click", function(){
+  let id = inputJoinId.value;
+
+  // errorMessage.innerHTML = "";
+  displayError.style.display = "none";
+
+  socket.emit("join-room", id);
+})
+
+
+
+
+socket.on("room-created", id => {
+  playerId = 1;
+  roomId = id;
+
+  startScreen.style.display = "none";
+  gameScreen.style.display = "block";
+
+  nextPlayer.innerHTML='waiting for the other player to join ...'
+})
+
+socket.on("room-joined", id => {
+  playerId = 2;
+  roomId = id;
+
+  playerOneConnected = true;
+  playerTwoConnected = true;
+  // playerJoinTheGame(1);
+  // setWaitMessage(false);
+
+  startScreen.style.display = "none";
+  gameScreen.style.display = "block";
+})
+
+socket.on('reflect', attr => {
+  console.log(attr);
+  var sym = 'X';
+  document.querySelector(`[data-cell-index="${attr}"]`).innerHTML = `<p>${sym}</p>`;
+})
+socket.on('clear', ()=> {
+  console.log('Clearing the screen')
+  nextPlayer.innerHTML='';
+})
 
 function handleAnyClick(event) {
   // const cellNumber = parseInt(event.target.getAttribute("data-cell-index"));
@@ -20,8 +94,9 @@ function handleAnyClick(event) {
   // event.target.innerHTML = currentPlayer;
   // console.log(event.target.getAttribute('data-cell-index'));
   // console.log(typeof event.target.getAttribute('data-cell-index'));
-
-  socket.emit('change', event.target.getAttribute('data-cell-index'));
+  console.log('Clicked')
+  
+  socket.emit('make-move', event.target.getAttribute('data-cell-index'), roomId);
   // gameStates[cellNumber] = currentPlayer;
   // cellsRemaining = cellsRemaining - 1;
   // validateCell();
--- server.js
+++ server.js
@@ -6,20 +6,60 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const port = process.env.PORT || 3000;
 
+const { userConnected, connectedUsers, initializeChoices, moves, makeMove, choices } = require("./utils/users");
+const { createRoom, joinRoom, exitRoom, rooms } = require("./utils/rooms");
+
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.sendFile('/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.join('groom');
+  console.log('new user');
+  // console.log(socket.id + ' ' + socket.client.id)
+  socket.on("create-room", (roomId) => {
+    if (rooms[roomId]) {
+      socket.emit("display-error", "This room already exists !!");
+    } else {
+      userConnected(socket.id);
+      createRoom(roomId, socket.id);
+      playerOneConnected = true;
+      socket.emit("room-created", roomId);
+      // socket.emit("player-1-connected");
+      socket.join(roomId);
+    }
+  })
 
-  socket.on('change', attr=> {
-    io.in('groom').emit('reflect',attr);
+
+  socket.on("join-room", roomId => {
+    if (!rooms[roomId]) {
+      socket.emit("display-error", "This room doesn't exist !!!");
+    } else {
+      userConnected(socket.id);
+      joinRoom(roomId, socket.id);
+      socket.join(roomId);
+
+      socket.emit("room-joined", roomId);
+      // socket.emit("player-2-connected");
+      // socket.broadcast.to(roomId).emit("player-2-connected");
+      // initializeChoices(roomId);
+      let other = rooms[roomId][0];
+      console.log(other);
+      io.to(other).emit('clear');
+    }
+  })
+
+  socket.on('make-move', (cno, rId) => {
+
+    const gameStates = rooms[rId][2];
+    // console.log(gameStates);
+
+    if (gameStates[cno] != '') return;
+    else
+      io.in(rId).emit('reflect', cno);
   })
 });
 
 server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
-});
+})
